ca3b8efc638f46f16b1d1de163141ecda32ad1ce
gamers145 <99580785+gamers145@users.noreply.github.com>
2023-09-17 09:46:45 +0530

    Create fitbot.py
    vertion 1 of fitbot

---
+++ fit_bot/fitbot.py
@@ -0,0 +1,22 @@
+import os
+import replicate
+
+os.environ["REPLICATE_API_TOKEN"] = "r8_DPxV9gTeE2DDsivU8ut3i3MW8YpgQ0Q0MYrpA"
+
+# Prompts
+pre_prompt = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+prompt_input = "name some protien rich food"
+
+# Generate LLM response
+output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # LLM model
+                        input={"prompt": f"{pre_prompt} {prompt_input} Assistant: ", # Prompts
+                        "temperature":0.1, "top_p":0.9, "max_length":128, "repetition_penalty":1})  # Model parameters
+
+output
+
+full_response = ""
+
+for item in output:
+  full_response += item
+
+print(full_response)
